acd29bf3b3dcda7572925e49962730c83f100254
Rishikesh-Reddy <20R21A6704@mlrinstitutions.ac.in>
2022-12-04 17:26:31 +0530

    Changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { Todo } = require("./models");
+Todo.sync();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
